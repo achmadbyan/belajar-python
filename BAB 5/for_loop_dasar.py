f02013ad2fe8c3c981bcacdbc70bcd045d42bc81
achmadbyan <ahmadbiyantuban@gmail.com>
2026-09-11 07:24:58 +0700

    Add files via upload

---
+++ BAB 5/for_loop_dasar.py
@@ -0,0 +1,12 @@
+#sintaks for loop
+#for variabel in urutan:
+# #blok kode yang diulang
+    
+#contoh 1: cetak angka 1-5
+for i in range(1, 6):
+    print("angka:", i)
+    
+#contoh 2: iterasi karakter dalam string
+for huruf in "PYTHON":
+    print(huruf, end=" ")
+#output: PYTHON
---
+++ BAB 5/while_loop_dasar.py
@@ -0,0 +1,11 @@
+#sintaks while loop
+#while kondisi:
+#blok kode yang diulang 
+#pastikan ada yang menubah kondisi
+
+#contoh: hitung mundur dari 5
+n = 5
+while n > 0:
+    print("hitung mundur:", n)
+    n -= 1    # n = n - 1  (kondisi akhirnya False)
+print("MULAI!")
