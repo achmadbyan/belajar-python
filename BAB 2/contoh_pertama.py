3e20c96ba9701f06cc32fd67d44e8073f61ff5d0
achmadbyan <ahmadbiyantuban@gmail.com>
2026-09-11 07:23:18 +0700

    Add files via upload

---
+++ BAB 2/contoh_pertama.py
@@ -0,0 +1,6 @@
+# ini adalah komentar - tidak dijalankan oleh Python
+# Program: Salam Perkenalan 
+
+print("Halo, Dunia")         #menampilkan teks
+print("Nama saya: Byan")     #setiap primt() = satu baris output
+print("Versi: 3.x")          #tanda kutip bisa ganda atau tunggal
---
+++ BAB 2/program_pertama.py
@@ -0,0 +1,11 @@
+# Program Pertamaku
+# Nama: [Byan]
+# Kelas: X RPL 1
+
+print("===============================")
+print("  SELAMAT DATANG DI PYTHON!  ")
+print("===============================")
+print()
+print("Program ini dibuat oleh:", "[Byan]")
+print("Sekolah: SMK TJP Tuban")
+print("Tahun:", 2026)
